f23c74d693f1ecc0df02ee453781cf364f8ab8ce
marcos <sebastianimfeld@gmail.com>
2020-03-10 22:11:39 -0300

    fixing function

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
 
 var clock;
-var distance;
 
-function timerPomodoro() {
+var distance;
+var distanceInitial;
+
+function timer(d) {
+
+	if(d === 1500000 || d === 300000 || d === 600000) distanceInitial = d;
 
 	clearInterval(clock);
-
-	distance = 1500000;
+	
+	distance = d;
 
 	clock = setInterval(function() {
-
 
 		let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 		let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
 		seconds = seconds >= 10 ? seconds : `0${seconds}`;
+		minutes = minutes >= 10 ? minutes : `0${minutes}`;
 
 		document.getElementById("clock").innerHTML = minutes + ":" + seconds;
 		document.querySelector("title").innerHTML = "(" + minutes + ":" + seconds + ") TomatoTimer";
@@ -22,83 +26,47 @@ function timerPomodoro() {
 		distance -= 1000;
 
 		if (distance < 0) {
-	    	clearInterval(clock);
-	    	document.getElementById("clock").innerHTML = "25:00";
+			clearInterval(clock);
+
+			let finalTextTimer = "";
+			
+			if(d == 1500000) finalTextTimer = "25:00";
+			else if(d == 300000) finalTextTimer = "05:00";
+			else finalTextTimer = "10:00";
+					
+			document.getElementById("clock").innerHTML = finalTextTimer;
+			document.querySelector("title").innerHTML = "Buzzzzzzz!";
 	  	}
-
 	}, 1000);
-}
-
-function timerShortBreak() {
-	clearInterval(clock);
-
-	distance = 300000;
-	
-	clock = setInterval(function() {
-
-
-		let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-		let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-		seconds = seconds >= 10 ? seconds : `0${seconds}`;
-
-		document.getElementById("clock").innerHTML = `0${minutes}` + ":" + seconds;
-		document.querySelector("title").innerHTML = "(" + `0${minutes}` + ":" + seconds + ") TomatoTimer";
-
-		distance = distance - 1000;
-
-		if (distance < 0) {
-	    	clearInterval(clock);
-	    	document.getElementById("clock").innerHTML = "05:00";
-	  	}
-
-	}, 1000);
-}
-
-function timerLongBreak() {
-
-	clearInterval(clock);
-
-	distance = 600000;
-	
-	clock = setInterval(function() {
-
-
-		let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-		let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-		seconds = seconds >= 10 ? seconds : `0${seconds}`;
-		minutes = minutes === 10 ? minutes : `0${minutes}`;
-
-		document.getElementById("clock").innerHTML = minutes + ":" + seconds;
-		document.querySelector("title").innerHTML = "(" + minutes + ":" + seconds + ") TomatoTimer";
-		
-		distance = distance - 1000;
-
-		if (distance < 0) {
-	    	clearInterval(clock);
-	    	document.getElementById("clock").innerHTML = "10:00";
-	  	}
-
-	}, 1000);
+	console.log(`distanceInitial desde timer ${distanceInitial}`);
 }
 
 function stopAnyTimer() {
 	clearInterval(clock);
+	console.log("distance desde stop: " + distanceInitial);
 }
 
+function startTimer(d) {
+	timer(d);
+	console.log("distance desde start: " + distanceInitial);
+}
 
-let btnPomodoro = document.getElementById('pomodoro');
-btnPomodoro.addEventListener("click", timerPomodoro);
-
-let btnShortBreak = document.getElementById('shortbreak');
-btnShortBreak.addEventListener("click", timerShortBreak);
-
-let btnLongBreak = document.getElementById('longbreak');
-btnLongBreak.addEventListener("click", timerLongBreak);
+function resetTimer() {
+	clearInterval(clock);
+	distance = distanceInitial;
+	console.log(`distanceInitial desde reset ${distanceInitial}`);
+	let finalTextTimer = "";
+			
+	if(distanceInitial == 1500000) finalTextTimer = "25:00";
+	else if(distanceInitial == 300000) finalTextTimer = "05:00";
+	else if(distanceInitial == 600000) finalTextTimer = "10:00";
+					
+	document.getElementById("clock").innerHTML = finalTextTimer;
+}
 
 let btnStop = document.getElementById('stop');
 btnStop.addEventListener("click", stopAnyTimer);
+
 
 
 /**
